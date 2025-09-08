13925833d4199ad552c15e1e88ff4538b8c97677
Sadiq Hussain <sa7434542@gmail.com>
2025-09-08 17:14:34 +0500

    edit complain

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Complaints from "./pages/Complaints";
 import ComplaintForm from "./components/complaints/ComplaintForm";
+import ComplaintStatus from "./components/complaints/ComplaintStatus";
+import ComplaintPage from "./components/complaints/ComplaintPage";
 
 const MainFunction = () => {
   return (
@@ -29,6 +31,7 @@ const router = createBrowserRouter([
       { path: "/complain", element: <Complaints /> },
       { path: "/contact-us", element: <ContactUs /> },
       { path: "/complainform", element: <ComplaintForm /> },
+      { path: "/complainstatus", element: <ComplaintForm /> },
     ],
   },
   { path: "/login", element: <Login /> },
