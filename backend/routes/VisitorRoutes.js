b17e01cf9184ec145814935b13dd9a357118b421
shakeel7521951 <shakeel7521951@gmail.com>
2025-09-24 22:58:14 -0700

    visitor api's

--- backend/routes/VisitorRoutes.js
+++ backend/routes/VisitorRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { addVisitor, getVisitors } from "../controllers/visitorController";
+import { addVisitor, deleteVisitor, getVisitorById, getVisitors, updateVisitorStatus } from "../controllers/visitorController.js";
 const router = express.Router();
 
 router.post("/add-visitor",addVisitor);
 router.get("/get-visitors",getVisitors);
+router.get("/get-visitor-by-id",getVisitorById);
+router.get("/update-visitor-status",updateVisitorStatus);
+router.delete("/delete-visitor",deleteVisitor);
 
 export default router;
