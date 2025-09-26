d3b14287ee1c884edb773f1b84099179e99c2d87
shakeel7521951 <shakeel7521951@gmail.com>
2025-09-26 05:04:26 -0700

    update sendmail

--- backend/routes/FormRoutes.js
+++ backend/routes/FormRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createForm, deleteForm, getAllForms, getFormHTML, singleForm, updateForm } from "../controllers/FormsController.js";
-import auth from "../middlewares/AuthMiddleWare.js";
+// import auth from "../middlewares/AuthMiddleWare.js";
 const router = express.Router();
 
 router.post("/createForm", createForm);
