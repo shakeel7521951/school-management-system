a7556d84987123d521b4a86bfc6007be47b3b6a6
shakeel7521951 <shakeel7521951@gmail.com>
2025-09-26 04:42:06 -0700

    update gmail credientials

--- backend/utils/SendMail.js
+++ backend/utils/SendMail.js
@@ -1,30 +1,30 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "bmxadventure8@gmail.com",
-    pass: "yoie uzhf crai ccks",
-  },
-  secure: true,
-  timeout: 10000,
+    service: 'gmail',
+    auth: {
+        user: "shakeel7521951@gmail.com",
+        pass: "irfm pzgh kebw uvfg"
+    },
+    secure: true,
+    timeout: 10000
 });
 
 const SendMail = async (email, subject, text) => {
-  try {
-    const mailOptions = {
-      from: "bmxadventure8@gmail.com", 
-      to: email, 
-      subject: subject,
-      html: text,
-    };
-    await transporter.sendMail(mailOptions);
-  } catch (error) {
-    console.error("❌ Email sending failed:", error);
-    throw new Error("Failed to send mail");
-  }
+    try {
+        const mailOptions = {
+            from: 'shakeeldev.tech@gmail.com',
+            to: email,
+            subject: subject,
+            html: text
+        };
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.error("❌ Email sending failed:", error);
+        throw new Error("Failed to send mail");
+    }
 };
 
 export default SendMail;
