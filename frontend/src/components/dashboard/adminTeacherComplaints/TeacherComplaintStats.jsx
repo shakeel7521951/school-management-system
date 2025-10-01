fc04eb040f1ef06728b4749dc49f686a1f232bca
shakeel7521951 <shakeel7521951@gmail.com>
2025-10-01 00:13:53 -0700

    set complaint filters on admin side

--- frontend/src/components/dashboard/adminTeacherComplaints/TeacherComplaintStats.jsx
+++ frontend/src/components/dashboard/adminTeacherComplaints/TeacherComplaintStats.jsx
@@ -6,17 +6,17 @@ const TeacherComplaintStats = ({ complaints }) => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
             <div className="group bg-white p-5 rounded-2xl shadow-md border-l-4 border-red-400 transition-all duration-300 hover:shadow-lg hover:-translate-y-1">
                 <h3 className="text-gray-500 text-sm font-medium">Pending Complaints</h3>
-                <p className="text-2xl font-bold text-gray-800">{complaints.filter((c) => c.status === "Pending").length}</p>
+                <p className="text-2xl font-bold text-gray-800">{complaints.filter((c) => c.status === "Pending" || c.status === "pending").length}</p>
             </div>
 
             <div className="group bg-white p-5 rounded-2xl shadow-md border-l-4 border-yellow-400 transition-all duration-300 hover:shadow-lg hover:-translate-y-1">
                 <h3 className="text-gray-500 text-sm font-medium">In Progress</h3>
-                <p className="text-2xl font-bold text-gray-800">{complaints.filter((c) => c.status === "In Progress").length}</p>
+                <p className="text-2xl font-bold text-gray-800">{complaints.filter((c) => c.status === "in progress").length}</p>
             </div>
 
             <div className="group bg-white p-5 rounded-2xl shadow-md border-l-4 border-green-400 transition-all duration-300 hover:shadow-lg hover:-translate-y-1">
                 <h3 className="text-gray-500 text-sm font-medium">Resolved</h3>
-                <p className="text-2xl font-bold text-gray-800">{complaints.filter((c) => c.status === "Resolved").length}</p>
+                <p className="text-2xl font-bold text-gray-800">{complaints.filter((c) => c.status === "Resolved" || c.status === "resolved").length}</p>
             </div>
 
             <div className="group bg-white p-5 rounded-2xl shadow-md border-l-4 border-blue-400 transition-all duration-300 hover:shadow-lg hover:-translate-y-1">
