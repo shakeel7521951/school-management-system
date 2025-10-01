bd29f1f8a68df766726d4cb83ddfca04475ff3a5
fakharabbasbhatti <fakharabbasbhatti333@gmail.com>
2025-10-01 00:29:05 -0700

    languages change english and to arbic

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -7,16 +7,21 @@ import home_en from "./en/Home.json";
 import home_ar from "./ar/Home.json";
 import contact_en from "./en/Contact.json";
 import contact_ar from "./ar/Contact.json";
+import news_en from "./en/News.json";
+import news_ar from "./ar/News.json";
 
 const resources = {
   en: {
     home: home_en,
-    contact:contact_en
+    contact:contact_en,
+    news:news_en,
   },
   ar: {
     home: home_ar,
-    contact:contact_ar
+    contact:contact_ar,
+    news:news_ar
   },
+  
 };
 
 i18n
