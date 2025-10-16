87266189f2cc9ec50365dc5ec81dd6389fdd5308
shakeel7521951 <shakeel7521951@gmail.com>
2025-10-16 04:21:57 -0700

    update

--- backend/models/Visitor.js
+++ backend/models/Visitor.js
@@ -9,6 +9,10 @@ const visitorSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
     phone: {
         type: String,
         required: true
