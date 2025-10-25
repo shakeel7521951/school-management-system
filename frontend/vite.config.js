87624e45b3e063b61afa45af7563cb17cb05b618
shakeel7521951 <shakeel7521951@gmail.com>
2025-10-24 23:36:28 -0700

    Update vite.config.js to exclude mongoose from frontend bundle

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
-      external: ["fs", "path"], // add any Node modules here
+      external: ["mongoose"], // exclude mongoose from frontend bundle
     },
   },
 });
